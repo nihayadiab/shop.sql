c6893d3244fb8372dfa7fd2910c91209d49647e4
nihayadiab <nihayadiab@gmail.com>
2025-11-02 09:51:17 +0200

    Create sho2.sql

---
+++ sho2.sql
@@ -0,0 +1,10 @@
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE,
+    phone VARCHAR(20),
+    city VARCHAR(50),
+    country VARCHAR(50),
+    registration_date DATE DEFAULT CURRENT_DATE
+)
